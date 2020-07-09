3d200c8a74619255ace5646de7251d54ce3185cc
Avelcorvin <52276079+Avelcorvin@users.noreply.github.com>
2020-07-09 09:11:29 +0300

    add first files

---
+++ app.js
@@ -0,0 +1,22 @@
+const express =  require('express')
+const app = express()
+const port = 3000
+
+
+app.set('trust proxy','127.0.0.0/8')   
+ 
+
+app.get('/',(req,res)=>{
+
+    console.log(req.headers)
+    console.log(req.ips)
+    console.log(req.connection.remoteAddress)
+    res.json(req.ip)
+})
+
+
+
+app.listen(
+    port,(req,res)=>{
+    console.log('server is start')
+})
